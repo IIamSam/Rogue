7640e51c885651374427bfe0a80c827fe4a77070
Sam <sebowmanx@gmail.com>
2015-06-01 02:27:16 -0600

    curses and close no .gitignore cus idk how to

--- roguelike/roguelike/roguelike.cpp
+++ roguelike/roguelike/roguelike.cpp
@@ -6,7 +6,7 @@ int main() {
 	initscr();
 	clear();
 	// Print a string on screen
-	printw("Seems that you can use ncurses ...\nPress any key to exit!");
+	printw("Seems that you can use curses ...\nPress any key to exit!");
 
 	// Wait until the user press a key
 	getch();
